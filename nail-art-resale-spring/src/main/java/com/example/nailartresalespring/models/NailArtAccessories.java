db1da36f21ff36feb91fb095206c52150213d012
stefanibaker <its.stefani@gmail.com>
2020-08-31 11:43:20 -0400

    Models created.

---
+++ nail-art-resale-spring/src/main/java/com/example/nailartresalespring/models/NailArtAccessories.java
@@ -0,0 +1,65 @@
+package com.example.nailartresalespring.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="NailArtAccessories")
+public class NailArtAccessories {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String type;
+
+    @Column
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public NailArtAccessories() {
+    }
+}
---
+++ nail-art-resale-spring/src/main/java/com/example/nailartresalespring/service/NailPolishService.java
@@ -0,0 +1,10 @@
+package com.example.nailartresalespring.service;
+
+import com.example.nailartresalespring.models.NailPolish;
+import org.springframework.http.HttpStatus;
+
+public interface NailPolishService {
+    public Iterable<NailPolish> listNailPolish();
+    public NailPolish createNailPolish(NailPolish nailPolish);
+    public HttpStatus deleteNailPolish(Long id);
+}
